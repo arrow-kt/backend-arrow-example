6ab3d2228de72dfec57ae271d72c16207b6a950e
Alejandro Serrano <trupill@gmail.com>
2021-11-15 16:20:19 +0100

    Better pure validation

--- src/main/kotlin/io/arrow/example/validation/Structure.kt
+++ src/main/kotlin/io/arrow/example/validation/Structure.kt
@@ -1,11 +1,13 @@
 package io.arrow.example.validation
 
+import arrow.core.NonEmptyList
+import arrow.core.Validated
 import arrow.core.ValidatedNel
 import arrow.core.andThen
 import arrow.core.invalidNel
-import arrow.core.sequenceValidated
 import arrow.core.traverseValidated
 import arrow.core.validNel
+import arrow.core.zip
 import io.arrow.example.Entry
 import io.arrow.example.Order
 
@@ -17,23 +19,21 @@ enum class ValidateStructureProblem {
 }
 
 fun Order.validateStructure(): ValidatedNel<ValidateStructureProblem, Order> =
-  validateAll(
-    entries.check(ValidateStructureProblem.EMPTY_ORDER, List<Entry>::isNotEmpty),
-    entries.traverseValidated(Entry::validateStructure)
-  )
+  entries.check(ValidateStructureProblem.EMPTY_ORDER, List<Entry>::isNotEmpty).andThen {
+    entries.traverseValidated(Entry::validateEntry)
+  }.map(::Order)
 
-fun Entry.validateStructure(): ValidatedNel<ValidateStructureProblem, Entry> =
-  validateAll(
-    id.check(ValidateStructureProblem.EMPTY_ID, String::isNotEmpty).andThen {
-      id.check(ValidateStructureProblem.INCORRECT_ID) {
-        Regex("^ID-(\\d){4}\$").matches(it)
-      }
-    },
-    amount.check(ValidateStructureProblem.NON_POSITIVE_AMOUNT) { it > 0 }
-  )
+fun Entry.validateEntry(): ValidatedNel<ValidateStructureProblem, Entry> =
+  id.validateEntryId()
+    .zip(amount.validateEntryAmount(), ::Entry)
+
+fun String.validateEntryId(): ValidatedNel<ValidateStructureProblem, String> =
+  check(ValidateStructureProblem.EMPTY_ID, String::isNotEmpty).andThen {
+    check(ValidateStructureProblem.INCORRECT_ID) { Regex("^ID-(\\d){4}\$").matches(it) }
+  }
+
+fun Int.validateEntryAmount(): Validated<NonEmptyList<ValidateStructureProblem>, Int> =
+  check(ValidateStructureProblem.NON_POSITIVE_AMOUNT) { it > 0 }
 
 fun <E, A> A.check(problem: E, predicate: (A) -> Boolean): ValidatedNel<E, A> =
   if (predicate(this)) this.validNel() else problem.invalidNel()
-
-fun <E, A> A.validateAll(vararg validations: ValidatedNel<E, *>): ValidatedNel<E, A> =
-  validations.toList().sequenceValidated().map { this }
